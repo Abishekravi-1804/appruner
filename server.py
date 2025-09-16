e4f5822620d54dbd9997756da12cf164569e1ce9
Abishek Ravi <91658319+Abishekravi-1804@users.noreply.github.com>
2025-09-16 18:42:39 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
 from wsgiref.simple_server import make_server
- from pyramid.config import Configurator
- from pyramid.response import Response
- import os
- def hello_world(request): 
-    name = os.environ.get('NAME') 
-    if name == None or len(name) == 0: 
-        name = "world" 
-    message = "Hello, " + name + "!\n" 
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
+
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if not name: 
+        name = "world"
+    message = "Hello, " + name + "!\n"
     return Response(message)
- if __name__ == '__main__': 
-    port = int(os.environ.get("PORT")) 
-    with Configurator() as config: 
-        config.add_route('hello', '/') 
-        config.add_view(hello_world, route_name='hello') 
-        app = config.make_wsgi_app() 
-    server = make_server('0.0.0.0', port, app) 
+
+if __name__ == '__main__':
+    
+    port = int(os.environ.get("PORT", 8080))
+    
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+
+    print(f"Serving on port {port}...")  
+    server = make_server('0.0.0.0', port, app)
     server.serve_forever()
